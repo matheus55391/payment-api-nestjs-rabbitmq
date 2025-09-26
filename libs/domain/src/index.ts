e62487ba0031e8a2e5cb49d1230ac04c700e3c3b
matheus55391 <matheus.manulo55@gmail.com>
2025-09-26 10:24:05 -0300

    feat: add domain module exports and update TypeScript configuration

---
+++ libs/domain/src/index.ts
@@ -0,0 +1,13 @@
+// Constants
+export * from './constants/api-routes.constants'
+export * from './constants/http-status.constants'
+export * from './constants/microservices.constants'
+export * from './constants/queue.constants'
+
+// Entities
+export * from './entities/client.entity'
+export * from './entities/order-item.entity'
+export * from './entities/order.entity'
+export * from './entities/payment.entity'
+export * from './entities/product.entity'
+export * from './entities/restaurant.entity'
